f820fef62426daa4a69a5ab00c41d64327719a2c
ChrisCross <60717304+ChrisCross1337@users.noreply.github.com>
2025-06-16 07:48:20 -0500

    Update keymap.c

--- layouts/default/split_3x5_3/default_split_3x5_3/keymap.c
+++ layouts/default/split_3x5_3/default_split_3x5_3/keymap.c
@@ -23,5 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                                    KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
+const key_override_t *key_overrides[] = {
+    &my_override_1,
+    &my_override_2,
+};
     )
 };
